3130ffb920b3ded420f12bbded2330ae281b609b
Nick <Nick@DESKTOP-CD5UUTH>
2022-09-28 23:56:48 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = Console.ReadLine();
+            try
+            {
+                int n = Convert.ToInt32(s);
+                Console.WriteLine($"Ви ввели число {n}");
+            }
+            catch
+            {
+                Console.WriteLine("Не коректне введення");
+            }
+        }
+    }
+}
